d23a7ea89a3641ad9fd45b33fac4266e88d64200
helpbringer6-ui <bdtwalalo@hotmail.com>
2025-10-07 13:48:05 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Willkommen zur Augmented Reality Hausplanung!</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
